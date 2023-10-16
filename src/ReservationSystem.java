d05e6bcf88e807a859498cc84d4e3b98c29c0fcf
lucaswestphal <lucas_westphal25@milton.edu>
2023-10-15 20:27:48 -0400

    I added code to check if a reservation is null in display reservations, and I made sure that my sort reservations method checked for empty reservations

--- src/ReservationSystem.java
+++ src/ReservationSystem.java
@@ -1,5 +1,4 @@
 public class ReservationSystem {
-
     int reservationsMade = 0;
 
     Reservation[] reservations = new Reservation[10];
@@ -11,11 +10,12 @@ public class ReservationSystem {
     public ReservationSystem(){
         //Reservation r = new Reservation(1, 1, 2, 2);
         //r.makeString();
-        for (int i=0;i<reservations.length;i++){
-            addReservation(((int)(i*Math.random())+2), i*Math.random(), (int)(i*Math.random()), (long)(i*Math.random()));
+        for (int i=0;i<reservations.length-1;i++){
+            addReservation2(((int)(i*Math.random())+2), i*Math.random(), (int)(i*Math.random()), (long)(i*Math.random()));
         }
         displayReservations();
         sortReservations();
+        System.out.println("-----------------------------");
         displayReservations();
 
     }
@@ -25,9 +25,23 @@ public class ReservationSystem {
         reservationsMade++;
     }
 
-    public void displayReservations(){
+    public void addReservation2 (int numPeople, double time, int priority, long timeMade){
         for (int i=0;i<reservations.length;i++){
-            reservations[i].makeString();
+            if (reservations[i] == null){
+                reservations[i] = new Reservation(numPeople, time, priority, timeMade);
+                break;
+            }
+        }
+
+    }
+
+    public void displayReservations(){
+        for (Reservation r : reservations){
+            if (r == null){
+                System.out.println("reservation does not exist");
+            }else{
+                r.makeString();
+            }
         }
     }
 
@@ -35,15 +49,20 @@ public class ReservationSystem {
         Reservation filler = new Reservation(0,0,0,0);
         for (int a=0;a<reservations.length-1;a++) {
             for (int b=0; b < reservations.length-1; b++) {
-                if (reservations[b].timeMade > reservations[b + 1].timeMade) {
-                    filler = reservations[b + 1];
-                    reservations[b + 1] = reservations[b];
-                    reservations[b] = filler;
+                if (reservations[b] != null && reservations[b+1] != null){
+                    if (reservations[b].timeMade > reservations[b + 1].timeMade) {
+                        filler = reservations[b + 1];
+                        reservations[b + 1] = reservations[b];
+                        reservations[b] = filler;
+                    }
                 }
             }
         }
         for (int i=0;i<reservations.length;i++){
-            reservations[i].priority = i;
+            if (reservations[i] != null){
+                reservations[i].priority = i;
+            }
+
         }
     }
 }
